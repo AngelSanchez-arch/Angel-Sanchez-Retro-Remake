c5e15168fb7ef4ab8e3ecbf214fd0c58c6243de6
MUKILTEO\1720986 <Kirby336242@gmail.com>
2024-09-23 13:22:50 -0700

    hi

--- Retro Remake/Assets/Script/MovmentPM.cs
+++ Retro Remake/Assets/Script/MovmentPM.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MovmentPM : MonoBehaviour
 {
     public KeyCode left = KeyCode.A, right = KeyCode.D, up = KeyCode.W, down = KeyCode.S; //jump = KeyCode.w
-    public float speed = 10, jumpHeight = 15;
+    public float speed = 10, jumpHeight = 15, maxSpeed = 10;
    
     private Rigidbody2D _rb;
     // Start is called before the first frame update
@@ -24,24 +24,31 @@ public class MovmentPM : MonoBehaviour
         if (Input.GetKey(left)) //Check for player holding down left button
         {
             // Get the GameObject's rigidbody2d component and set its velocity to the left at the given speed
-            _rb.velocity = Vector2.left * speed;
+            _rb.velocity = new Vector2(-speed, _rb.velocity.y);
+           // _rb.velocity = Vector2.left * speed;
         }
         if (Input.GetKey(right)) //Check for player holding down right button
         {
             // Get the GameObject's rigidbody2d component and set its velocity to the left at the given speed
-            _rb.velocity = Vector2.right * speed;
+            _rb.velocity = new Vector2(speed, _rb.velocity.y);
+            //_rb.velocity = Vector2.right * speed;
         }
 
         if (Input.GetKey(up)) //Check for player holding down up button
         {
             // Get the GameObject's rigidbody2d component and set its velocity to the left at the given speed
-            _rb.velocity = Vector2.up * speed;
+            _rb.velocity = new Vector2(_rb.velocity.x, speed);
+            //_rb.velocity = Vector2.up * speed;
         }
         if (Input.GetKey(down)) //Check for player holding down button
         {
             // Get the GameObject's rigidbody2d component and set its velocity to the left at the given speed
-            _rb.velocity = Vector2.down * speed;
+            _rb.velocity = new Vector2(_rb.velocity.x, -speed);
+            //_rb.velocity = Vector2.down * speed;
         }
+        
+
+
 
         //if (Input.GetKeyDown(jump))
         {
